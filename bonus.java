d35b8894c5534d0eedc72d2c38bc59aacb81741f
AlexandraPyaterikova <AlexandraPyaterikova@yandex.ru>
2021-03-09 23:49:15 +0300

    changes saved

---
+++ bonus.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    int income = 5000;
+    int bonus;
+    if (income >= 1000) {
+   bonus = income / 100 * 1;
+}
+else {
+bonus = 0;
+}
+System.out.println(bonus);}
+}
