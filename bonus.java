4044a8e510f70854a4274acba41b8a847ffd2b17
AlexandraPyaterikova <AlexandraPyaterikova@yandex.ru>
2021-03-10 00:23:32 +0300

    code added

--- bonus.java
+++ bonus.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-    int income = 5000;
-    int balance = 300;
-    int bonus;
-    if (income >= 1000) {
-   bonus = income / 100 * 1;
-}
-else {
-bonus = 0;
-}
-System.out.println(bonus);}
+        int income = 20;
+        int balance = 300;
+        int bonus;
+        if (income >= 1000) {
+            bonus = income / 100 * 1;
+        } else {
+            bonus = 0;
+        }
+        int totalbalance = income + bonus + balance;
+
+
+        System.out.println("bonus: " + bonus);
+        System.out.println("totalbalance: " + totalbalance);
+    }
 }
